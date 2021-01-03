eb4b647b5e38e99c817a35b98300de8974983f99
Toni Elise Latimer <watersignrituals@gmail.com>
2021-01-03 17:45:45 -0500

    changes to src/util/Windy.js & Windy-help.js...now getting 500 status when I submit post request to Windy API

--- src/util/Windy-help.js
+++ src/util/Windy-help.js
@@ -72,3 +72,95 @@ const Windy = {   //This one is with CORS solution added + headers based on rese
       });
     }
   }
+
+  const Windy = {   //added additional headers based on herokuapp suggestions
+    search() {
+      fetch("https://cors-anywhere.herokuapp.com/https://api.windy.com/api/point-forecast/v2", {
+          method: "POST",
+          headers: {
+              "Content-Type": "application/json",
+              "Accept": "application/json",
+              "Origin": "http://192.168.0.16:3000",
+              "X-Requested-With": "XMLHttpRequest"
+            },
+          body: {
+              "lat": 26.485170269658564,
+              "lon": -82.00530052609375,
+              "model": "gfs",
+              "parameters": ["temp", "precip", "wind", "windGust", "rh"],
+              "levels": ["surface"],
+              "key": "N72ErLMBgD4tEOUy5G695g5N1ofz3qdO"
+          }
+      }).then(response => {
+          if (response.ok) {
+              return response.json();
+          } else {
+              console.log('Error!');
+          }
+      }).then(jsonResponse => {
+          console.log(jsonResponse);
+      });
+    }
+  }
+
+  const Windy = {   //getting error: name_not_resolved after adding xhr.setRequestHeader() functions
+    search() {
+      const xhr = new XMLHttpRequest();
+      const url = "https://api.windy.com/api/point-forecast/v2"; //https://cors-anywhere.herokuapp.com/
+      //const header = {
+        //"Content-Type": "application/json",
+        //"Accept": "application/json",
+        //"Origin": "http://192.168.0.16:3000",
+        //"X-Requested-With": "XMLHttpRequest"
+      //}
+      const body = {
+        "lat": 26.484,
+        "lon": -82.004,
+        "model": "gfs",
+        "parameters": ["temp", "precip", "wind", "windGust", "rh"],
+        "levels": ["surface"],
+        "key": "N72ErLMBgD4tEOUy5G695g5N1ofz3qdO"
+      };
+      xhr.responseType = "json";
+      xhr.onreadystatechange = () => {
+          if (xhr.readystate === XMLHttpRequest.DONE) {
+              console.log(xhr.readystate);
+          }
+      };
+      xhr.open("POST", url);
+      xhr.setRequestHeader("Content-Type", "application/json");
+      xhr.setRequestHeader("Accept", "application/json");
+      //xhr.setRequestHeader("Origin", "http://192.168.0.16:3000");
+      xhr.setRequestHeader("X-Requested-With", "XMLHttpRequest");
+      xhr.send(body);
+    }
+}
+
+const Windy = {   //returning 500 error wih NO info, I think it actually may be a development server error
+search() {
+  fetch("https://api.windy.com/api/point-forecast/v2", {  //https://cors-anywhere.herokuapp.com/
+      method: "POST",
+      headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json",
+          //"X-Requested-With": "XMLHttpRequest"
+        },
+      body: {
+          "lat": 26.485170269658564,
+          "lon": -82.00530052609375,
+          "model": "gfs",
+          "parameters": ["temp", "precip", "wind", "windGust", "rh"],
+          "levels": ["surface"],
+          "key": "N72ErLMBgD4tEOUy5G695g5N1ofz3qdO"
+      }
+  }).then(response => {
+      if (response.ok) {
+          return response.json();
+      }
+  }).then(jsonResponse => {
+      console.log(jsonResponse);
+  }).catch(error => {
+      console.log(error);
+  });
+}
+}
--- src/util/Windy.js
+++ src/util/Windy.js
@@ -7,32 +7,37 @@
 //const levels = 'surface';
 //const key = 'N72ErLMBgD4tEOUy5G695g5N1ofz3qdO';
 
-const Windy = {   //got it to where it's returning a 500 error instead of 400!
+const Windy = {   //this XHR is formatted same as last fetch() request. I think it actually may be a 500 internal server error :(
     search() {
-      fetch('https://cors-anywhere.herokuapp.com/https://api.windy.com/api/point-forecast/v2', {
-          method: 'POST',
-          headers: {
-              'Content-Type': 'application/json',
-              'Accept': 'application/json'
-            },
-          body: {
-              'lat': 26.485170269658564,
-              'lon': -82.00530052609375,
-              'model': 'gfs',
-              'parameters': ['temp', 'precip', 'wind', 'windGust', 'rh'],
-              'levels': ['surface'],
-              'key': 'N72ErLMBgD4tEOUy5G695g5N1ofz3qdO'
+      const xhr = new XMLHttpRequest();
+      const url = "https://api.windy.com/api/point-forecast/v2"; //https://cors-anywhere.herokuapp.com/
+      //const header = {
+        //"Content-Type": "application/json",
+        //"Accept": "application/json",
+        //"Origin": "http://192.168.0.16:3000",
+        //"X-Requested-With": "XMLHttpRequest"
+      //}
+      const body = {
+        "lat": 26.484,
+        "lon": -82.004,
+        "model": "gfs",
+        "parameters": ["temp", "precip", "wind", "windGust", "rh"],
+        "levels": ["surface"],
+        "key": "N72ErLMBgD4tEOUy5G695g5N1ofz3qdO"
+      };
+      xhr.responseType = "json";
+      xhr.onreadystatechange = () => {
+          if (xhr.readystate === XMLHttpRequest.DONE) {
+              console.log(xhr.readystate);    //this line is returning: bootstrap:82 XHR finished loading: GET "http://192.168.0.16:3000/f6729087fcb0b98a8c27.hot-update.json"
           }
-      }).then(response => {
-          if (response.ok) {
-              return response.json();
-          } else {
-              console.log('Error!');
-          }
-      }).then(jsonResponse => {
-          console.log(jsonResponse);
-      });
+      };
+      xhr.open("POST", url);
+      xhr.setRequestHeader("Content-Type", "application/json");
+      xhr.setRequestHeader("Accept", "application/json");
+      //xhr.setRequestHeader("Origin", "http://192.168.0.16:3000");
+      //xhr.setRequestHeader("X-Requested-With", "XMLHttpRequest");
+      xhr.send(body);
     }
-  }
+}
 
 export default Windy;
